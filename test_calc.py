02cf914b67efd61ce000c3c8543d7a9206ba3042
sanyu <sanyumahajan@gmail.com>
2025-10-31 18:41:46 +0530

    Test

---
+++ test_calc.py
@@ -0,0 +1,13 @@
+from calc import add,sub,mul,div
+
+def test_add():
+    assert add(2,3) ==5
+
+def test_sub():
+    assert sub(5,3) ==2
+
+def test_mul():
+    assert add(2,3) ==6
+
+def test_div():
+    assert add(4,2) ==2
